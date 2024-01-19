355ccfd1794a25a46296912e8497eef4d7664d5c
MeZerius <metherius1@gmail.com>
2024-01-19 18:29:38 +0100

    Added poll grid design

--- pollApp/views.py
+++ pollApp/views.py
@@ -1,7 +1,13 @@
+from django.core.paginator import Paginator
 from django.shortcuts import render, redirect
 from .forms import RegistrationForm, LoginForm
 from django.contrib.auth import authenticate, login as auth_login
 from django.contrib.auth.decorators import login_required, user_passes_test
+from django.db.models import Count
+
+from django.utils import timezone
+
+from .models import Poll
 
 
 def anonymous_required(user):
@@ -44,8 +50,25 @@ def register(request):
 
 
 def activePolls(request):
-    return render(request, 'activePolls.html')
+    now = timezone.now()
+    polls = [poll for poll in Poll.objects.all() if not poll.is_expired]
+    for poll in polls:
+        poll.total_votes = sum(option.votes.count() for option in poll.options.all())
+        poll.end_time = poll.created_at + poll.active_time
+    paginator = Paginator(polls, 9)
+    page_number = request.GET.get('page')
+    polls = paginator.get_page(page_number)
+    return render(request, 'activePolls.html', {'polls': polls})
 
 
 def archivedPolls(request):
-    return render(request, 'archivePolls.html')
+    now = timezone.now()
+    polls = [poll for poll in Poll.objects.all() if poll.is_expired]
+    for poll in polls:
+        poll.total_votes = sum(option.votes.all().count() for option in poll.options.all())
+        poll.status = "Quorum isn't achieved" if poll.is_invalid else 'Expired'
+
+    paginator = Paginator(polls, 9)
+    page_number = request.GET.get('page')
+    polls = paginator.get_page(page_number)
+    return render(request, 'archivePolls.html', {'polls': polls})
